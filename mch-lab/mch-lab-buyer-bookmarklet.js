9eeb223fee5b89359dbd96b37b395f1f29580f2d
sahokun <kurukumawaru@gmail.com>
2024-08-17 12:26:55 +0900

    improve seller

--- mch-lab/mch-lab-buyer-bookmarklet.js
+++ mch-lab/mch-lab-buyer-bookmarklet.js
@@ -1,19 +1,23 @@
 
 javascript: (async function () {
-    const EPSILON = 1e-10;
-    const SCALE_FACTOR = 100;
-    const SCALE_FACTOR_PERCENT = 1 / SCALE_FACTOR;
-    const MAX_SCALE_FACTOR = 1000;
-    const TOLERANCE_PERCENT = 0.001; // 0.1%
-    const LOOP_LIMIT = 10;
+    const LOOP_LIMIT = 10; // ループ許容回数
+    const TOLERANCE_PERCENT = 0.001; // 誤差許容 0.1%
+    const SCALE_FACTOR = 100; // 計算有効桁数小数点以下二桁
+    const SCALE_FACTOR_PERCENT = 1 / SCALE_FACTOR; // 計算有効桁数小数点以下二桁%
 
-    const prices = await $nuxt.$store.$laboratoryService.getAllTokenPrices();
+    const EPSILON = 1e-10; // IEEE754丸め誤差対策用
+    const MAX_SCALE_FACTOR = 1000; // AMM有効桁数小数点以下三桁
 
+    // レート一覧
+    const TOKEN_PRICES = await $nuxt.$store.$laboratoryService.getAllTokenPrices();
+
+
+    // 結果を画面出力
     function setText(content) {
         const targetDiv = document.querySelector('.ammSwapModal__balance');
 
         const newPElement = document.createElement('p');
-        newPElement.textContent = `最適購入量: ${content}`; 
+        newPElement.textContent = `最適購入量 : ${content}`; 
         newPElement.classList.add('optimal-purchase-amount'); 
 
         const existingPElement = targetDiv.querySelector('p.optimal-purchase-amount');
@@ -25,26 +29,26 @@ javascript: (async function () {
         }
     }
 
+    // 対象素材取得
     function getMaterial() {
-        // class="ammSwapModal__token" の div を取得
         const targetDiv = document.querySelector('.ammSwapModal__token');
         if (!targetDiv) {
             throw new Error('素材が見つかりませんでした。');
         }
-        // img の src からファイル名を取得し、3桁の数字部分を抽出
+
+        // img の src からファイル名を取得し、3桁の数字部分を抽出->素材ID
         const imgSrc = targetDiv.querySelector('img').src;
         const filenameMatch = imgSrc.match(/\/(\d{3})\./);
-        const filenameNumber = filenameMatch ? filenameMatch[1] : null;
+        const materialId = filenameMatch ? filenameMatch[1] : null;
 
-        // p の文字を取得
-        const pText = targetDiv.querySelector('p').textContent;
+        // 名前の文字を取得
+        const materialName = targetDiv.querySelector('p').textContent;
 
-        console.log("3桁数字:", filenameNumber);
-        console.log("p の文字:", pText);
-
-        return { materialId: parseInt(filenameNumber), materialName: pText };
+        console.log("素材ID:", materialId, "素材名:", materialName);
+        return { materialId: parseInt(materialId), materialName: materialName };
     }
 
+    // レート一覧から指定素材の値を取得
     function getBuyerPrice(prices, tokenId) {
         for (const item of prices.tokenPricesMap) {
             if (item[0] === tokenId) {
@@ -54,16 +58,17 @@ javascript: (async function () {
         return null;
     }
 
+    // 最適購入量計算
     async function findOptimalPurchaseAmount(materialId, requiredAmount) {
         console.log("素材ID", materialId, "希望購入素材量:", requiredAmount);
 
         // 現在レートの取得
-        var initialPrice = getBuyerPrice(prices, materialId);
+        var initialPrice = getBuyerPrice(TOKEN_PRICES, materialId);
         console.log("現在価格:", initialPrice);
 
         // 上限素材購入量の概算 = ((現在レート * 必要量) + 1) / 現在レート
         var upperBound = Math.ceil(((Math.ceil(initialPrice * requiredAmount) + 1) / initialPrice) * SCALE_FACTOR) / SCALE_FACTOR;
-        console.log("上限素材購入量:", upperBound);
+        console.log("概算上限素材購入量:", upperBound);
 
         // 希望購入素材量での価格取得
         var minEstimate = await $nuxt.$store.$laboratoryService.estimatePurchaseAmount(materialId, requiredAmount * MAX_SCALE_FACTOR);
@@ -71,42 +76,43 @@ javascript: (async function () {
         console.log("目標価格:", targetPrice);
 
         var lowerBound = requiredAmount;
-
-        // ループは20回まで
-        for (let i = 0; i < LOOP_LIMIT; i++) {
-            console.log("ループ回数:", i + 1);
-            console.log("下限:", lowerBound, "上限:", upperBound, "目標:", targetPrice);
+        var i = 0;
+        while (true) {
+            console.log("ループ回数:", i + 1, "下限:", lowerBound, "上限:", upperBound, "目標:", targetPrice);
 
             // 中間値の計算
             let mid = Math.ceil(((lowerBound + upperBound) / 2) * SCALE_FACTOR) / SCALE_FACTOR;
-            console.log("中間値:", mid);
-
             // 中間値での価格取得
             let midEstimate = await $nuxt.$store.$laboratoryService.estimatePurchaseAmount(materialId, mid * MAX_SCALE_FACTOR);
             let midPrice = midEstimate.amount;
-            console.log("中間値の価格:", midPrice);
+            console.log("中間値:", mid, "中間値の価格:", midPrice);
 
             if (midPrice === targetPrice) {
                 // 中間値の価格が目標価格と同じ場合、下限を更新
                 lowerBound = mid;
-                console.log("midPrice === targetPrice");
-                console.log(`lowerBound${lowerBound} = mid${mid}`);
+                console.log(`中間値の価格が目標価格と同じなので下限を上げます。下限: ${mid}`);
             } else {
                 // 中間値の価格が目標価格より高い場合、上限を更新
-                console.log("midPrice > targetPrice");
                 upperBound = mid;
-                console.log(`upperBound${upperBound} = mid${mid}`);
+                console.log(`中間値の価格が目標価格より高いので上限を下げます。上限: ${mid}`);
             }
 
             // 下限と上限が隣り合った場合、ループ終了
-            let tolerance = Math.ceil(mid * TOLERANCE_PERCENT * MAX_SCALE_FACTOR) / MAX_SCALE_FACTOR;
-            console.log(`tolerance: ${tolerance}`);
-            console.log(`(upperBound - lowerBound): ${upperBound - lowerBound}, (0.01 + tolerance + EPSILON): ${(SCALE_FACTOR_PERCENT + tolerance + EPSILON)}`);
-            console.log(((upperBound - lowerBound) <= (SCALE_FACTOR_PERCENT + tolerance + EPSILON)));
-            if (upperBound - lowerBound <= SCALE_FACTOR_PERCENT + tolerance + EPSILON) {
-                console.log(`下限と上限が隣り合った場合、ループ終了`);
+            let toleranceAmount = mid * TOLERANCE_PERCENT;
+            const toleranceAmountWithScale = Math.ceil((SCALE_FACTOR_PERCENT + toleranceAmount + EPSILON) * MAX_SCALE_FACTOR) / MAX_SCALE_FACTOR;
+            const diff = Math.ceil(Math.abs(upperBound - lowerBound) * MAX_SCALE_FACTOR) / MAX_SCALE_FACTOR;
+            console.log(`計算量差分: ${diff}`, `許容誤差(${TOLERANCE_PERCENT*100}%): ${toleranceAmountWithScale}`);
+            if (diff <= toleranceAmountWithScale) {
+                console.log(`許容誤差内になったため計算終了`);
                 break;
             }
+
+            // ループ回数
+            if(i === LOOP_LIMIT - 1) {
+                alert("ループ回数上限を超えました。");
+                break;
+            }
+            i++;
         }
 
         // 最終的な購入可能量を返す
@@ -115,17 +121,17 @@ javascript: (async function () {
     }
 
     try {
+        // 素材情報
         const material = getMaterial();
-
+        // 購入量入力
         const inputValue = prompt(`${material.materialName} 希望購入量を入力してください:`);
-
-        // 入力値が数値であるか確認
         if (isNaN(inputValue)) {
             throw new Error("isNaN");
         }
-
         const requiredAmount = parseFloat(inputValue);
+        // 計算
         let optimalPurchaseAmount = await findOptimalPurchaseAmount(material.materialId, requiredAmount);
+        // 出力
         setText(optimalPurchaseAmount);
         alert(`最適な購入量: ${optimalPurchaseAmount}`);
     } catch (e) {
--- mch-lab/mch-lab-seller-bookmarklet.js
+++ mch-lab/mch-lab-seller-bookmarklet.js
@@ -1,39 +1,41 @@
 
 javascript: (async function () {
-    const EPSILON = 1e-10;
-    const SCALE_FACTOR = 100;
-    const SCALE_FACTOR_PERCENT = 1 / SCALE_FACTOR;
-    const MAX_SCALE_FACTOR = 1000;
-    const TOLERANCE_PERCENT = 0.001; // 0.1%
-    const LOOP_LIMIT = 10;
+    const LOOP_LIMIT = 10; // ループ許容回数
+    const TOLERANCE_PERCENT = 0.001; // 誤差許容 0.1%
+    const SCALE_FACTOR = 100; // 計算有効桁数小数点以下二桁
+    const SCALE_FACTOR_PERCENT = 1 / SCALE_FACTOR; // 計算有効桁数小数点以下二桁%
 
-    const prices = await $nuxt.$store.$laboratoryService.getAllTokenPrices();
+    const EPSILON = 1e-10; // IEEE754丸め誤差対策用
+    const MAX_SCALE_FACTOR = 1000; // AMM有効桁数小数点以下三桁
 
+    // レート一覧
+    const TOKEN_PRICES = await $nuxt.$store.$laboratoryService.getAllTokenPrices();
+
+    // 所持量取得
     function getAmount(){
         const div = document.querySelector('div[data-v-2819963c].ammSwapModal__balance');
         const allP = div.querySelectorAll('p');
 
-        const p = Array.from(allP).find(p => p.textContent.includes("所持量"));
+        const textContent = Array.from(allP).find(p => p.textContent.includes("所持量")).textContent;
 
-        // 前提として、p要素はすでに取得済みであるとします。
-        const textContent = p.textContent; 
-
-        // 正規表現を使って数値部分を抽出
-        const match = textContent.match(/[\d,]+(\.\d+)?/); // 小数点以下も考慮した正規表現
+        // 正規表現を使って数値部分を抽出(小数点ありカンマあり)
+        const match = textContent.match(/[\d,]+(\.\d+)?/);
 
         if (!match) {
-
          throw new Error("数値が見つかりませんでした。");   
         }
-        const number = parseFloat(match[0].replace(/,/g, ''));
-        return number;
+
+        const amount = parseFloat(match[0].replace(/,/g, ''));
+        console.log("所持量:", amount);
+        return amount;
     }
 
+    // 結果を画面出力
     function setText(content) {
         const targetDiv = document.querySelector('.ammSwapModal__balance');
 
         const newPElement = document.createElement('p');
-        newPElement.textContent = `最適売却量: ${content}`; 
+        newPElement.textContent = `最適売却量 : ${content}`; 
         newPElement.classList.add('optimal-seller-amount'); 
 
         const existingPElement = targetDiv.querySelector('p.optimal-seller-amount');
@@ -45,26 +47,26 @@ javascript: (async function () {
         }
     }
 
+    // 対象素材取得
     function getMaterial() {
-        // class="ammSwapModal__token" の div を取得
         const targetDiv = document.querySelector('.ammSwapModal__token');
         if (!targetDiv) {
             throw new Error('素材が見つかりませんでした。');
         }
-        // img の src からファイル名を取得し、3桁の数字部分を抽出
+
+        // img の src からファイル名を取得し、3桁の数字部分を抽出->素材ID
         const imgSrc = targetDiv.querySelector('img').src;
         const filenameMatch = imgSrc.match(/\/(\d{3})\./);
-        const filenameNumber = filenameMatch ? filenameMatch[1] : null;
+        const materialId = filenameMatch ? filenameMatch[1] : null;
 
-        // p の文字を取得
-        const pText = targetDiv.querySelector('p').textContent;
+        // 名前の文字を取得
+        const materialName = targetDiv.querySelector('p').textContent;
 
-        console.log("3桁数字:", filenameNumber);
-        console.log("p の文字:", pText);
-
-        return { materialId: parseInt(filenameNumber), materialName: pText };
+        console.log("素材ID:", materialId, "素材名:", materialName);
+        return { materialId: parseInt(materialId), materialName: materialName };
     }
 
+    // レート一覧から指定素材の値を取得
     function getSellerPrice(prices, tokenId) {
         for (const item of prices.tokenPricesMap) {
             if (item[0] === tokenId) {
@@ -74,11 +76,12 @@ javascript: (async function () {
         return null;
     }
 
+    // 最適売却量計算
     async function findOptimalSaleAmount(materialId, requiredAmount) {
         console.log("素材ID", materialId, "希望売却素材量:", requiredAmount);
 
         // 現在レートの取得
-        var initialPrice = getSellerPrice(prices, materialId);
+        var initialPrice = getSellerPrice(TOKEN_PRICES, materialId);
         console.log("現在価格:", initialPrice);
 
         // 下限素材購入量の概算 = ((現在レート * 必要量) - 1) / 現在レート
@@ -86,7 +89,7 @@ javascript: (async function () {
         if(lowerBound < 0) {
             lowerBound = 0;
         }
-        console.log("下限素材購入量:", lowerBound);
+        console.log("概算下限素材購入量:", lowerBound);
 
         // 希望売却素材量での価格取得
         var maxEstimate = await $nuxt.$store.$laboratoryService.estimateSaleAmount(materialId, requiredAmount * MAX_SCALE_FACTOR);
@@ -99,40 +102,36 @@ javascript: (async function () {
         var upperBound = requiredAmount;
         var i = 0;
         while (true) {
-            console.log("ループ回数:", i + 1);
-            console.log("上限:", upperBound, "下限:", lowerBound, "目標:", targetPrice);
+            console.log("ループ回数:", i + 1, "上限:", upperBound, "下限:", lowerBound, "目標:", targetPrice);
 
             // 中間値の計算
             let mid = Math.ceil(((upperBound + lowerBound) / 2) * SCALE_FACTOR) / SCALE_FACTOR;
-            console.log("中間値:", mid);
-
             // 中間値での価格取得
             let midEstimate = await $nuxt.$store.$laboratoryService.estimateSaleAmount(materialId, mid * MAX_SCALE_FACTOR);
             let midPrice = midEstimate.amount;
-            console.log("中間値の価格:", midPrice);
+            console.log("中間値:", mid, "中間値の価格:", midPrice);
 
             if (midPrice === targetPrice) {
                 // 中間値の価格が目標価格と同じ場合、上限を更新
                 upperBound = mid;
-                console.log("midPrice === targetPrice");
-                console.log(`upperBound${upperBound} = mid${mid}`);
+                console.log(`中間値の価格が目標価格と同じなので上限を下げます。上限: ${mid}`);
             } else {
                 // 中間値の価格が目標価格より低い場合、下限を更新
-                console.log("midPrice < targetPrice");
                 lowerBound = mid;
-                console.log(`lowerBound${lowerBound} = mid${mid}`);
+                console.log(`中間値の価格が目標価格より低いので下限を上げます。下限: ${mid}`);
             }
 
             // 下限と上限が隣り合った場合、ループ終了
-            let tolerance = Math.ceil(mid * TOLERANCE_PERCENT * MAX_SCALE_FACTOR) / MAX_SCALE_FACTOR;
-            console.log(`tolerance: ${tolerance}`);
-            console.log(`(upperBound - lowerBound): ${upperBound - lowerBound}, (0.01 + tolerance + EPSILON): ${(SCALE_FACTOR_PERCENT + tolerance + EPSILON)}`);
-            console.log(((upperBound - lowerBound) <= (SCALE_FACTOR_PERCENT + tolerance + EPSILON)));
-            if (upperBound - lowerBound <= SCALE_FACTOR_PERCENT + tolerance + EPSILON) {
-                console.log(`下限と上限が隣り合った場合、ループ終了`);
+            let toleranceAmount = mid * TOLERANCE_PERCENT;
+            const toleranceAmountWithScale = Math.ceil((SCALE_FACTOR_PERCENT + toleranceAmount + EPSILON) * MAX_SCALE_FACTOR) / MAX_SCALE_FACTOR;
+            const diff = Math.ceil(Math.abs(upperBound - lowerBound) * MAX_SCALE_FACTOR) / MAX_SCALE_FACTOR;
+            console.log(`計算量差分: ${diff}`, `許容誤差(${TOLERANCE_PERCENT*100}%): ${toleranceAmountWithScale}`);
+            if (diff <= toleranceAmountWithScale) {
+                console.log(`許容誤差内になったため計算終了`);
                 break;
             }
 
+            // ループ回数
             if(i === LOOP_LIMIT - 1) {
                 alert("ループ回数上限を超えました。");
                 break;
@@ -146,9 +145,17 @@ javascript: (async function () {
     }
 
     try {
+        // 素材情報
         const material = getMaterial();
-        const requiredAmount = parseFloat(getAmount());
+        // 売却量入力(初期値所持量)
+        const inputValue = prompt(`${material.materialName} 希望売却量を入力してください:`, getAmount());
+        if (isNaN(inputValue)) {
+            throw new Error("isNaN");
+        }
+        const requiredAmount = parseFloat(inputValue);
+        // 計算
         let optimalSaleAmount = await findOptimalSaleAmount(material.materialId, requiredAmount);
+        // 出力
         setText(optimalSaleAmount);
         alert(`最適な売却量: ${optimalSaleAmount}`);
     } catch (e) {
